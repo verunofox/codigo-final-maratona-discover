bede28940e33c70ecf9add736e1be98d6e84cd22
Veruno-fox <verunodl@gmail.com>
2021-02-06 09:25:12 -0300

    trocando para Math.round

--- scripts.js
+++ scripts.js
@@ -144,8 +144,8 @@ const Utils={
 
     },
     formatAmount(value){
-      value=Number(value.replace(/\,\./g,"")) *100
-      return value
+      value=value*100
+      return Math.round(value)
     },
 
     formatCurrency(value){
